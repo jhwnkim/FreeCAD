1f363b3d64cee32ea4940b4b4cea8a56f5cc4d10
Russell Johnson <47639332+Russ4262@users.noreply.github.com>
2020-09-12 01:05:56 -0500

    [Path] PocketShape - fix improper path generation
    Forum identification and discussion at https://forum.freecadweb.org/viewtopic.php?style=3&f=15&t=49513.

--- src/Mod/Path/PathScripts/PathPocketShape.py
+++ src/Mod/Path/PathScripts/PathPocketShape.py
@@ -685,8 +685,8 @@ class ObjectPocket(PathPocketBase.ObjectPocket):
                             PathLog.debug('LimitDepthToFace adj_final_dep: {}'.format(adj_final_dep))
                     else:
                         face.translate(FreeCAD.Vector(0, 0, obj.FinalDepth.Value - shpZMin))
-                    
-                    extent = FreeCAD.Vector(0, 0, start_dep - shpZMin + clrnc)  # adj_final_dep + clrnc)
+
+                    extent = FreeCAD.Vector(0, 0, abs(start_dep - shpZMin) + clrnc)
                     extShp = face.removeSplitter().extrude(extent)
                     self.removalshapes.append((extShp, False, 'pathPocketShape', useAngle, axis, start_dep, adj_final_dep))
                     PathLog.debug("Extent values are strDep: {}, finDep: {},  extrd: {}".format(start_dep, adj_final_dep, extent))
